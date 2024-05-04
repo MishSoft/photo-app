7809d7b78355e7cb46ff25e21db9c29bbe0c6d43
MishSoft <michaelmakaveli123@gmail.com>
2024-05-04 22:35:05 +0400

    Add auth function supabase, also add login signup componentns, get email verification link and autorisation function.

---
+++ app/components/AuthForm.jsx
@@ -0,0 +1,92 @@
+"use client";
+import React, { useState } from "react";
+import { supabase } from "../utils/supabaseClient";
+
+export default function AuthForm() {
+  const [isNewUser, setIsNewUser] = useState(false);
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [isSigningIn, setIsSigningIn] = useState(false);
+  const [isSigningUp, setIsSigningUp] = useState(false);
+
+  async function handleLogin(e) {
+    e.preventDefault();
+  }
+
+  async function handleSignUp(e) {
+    e.preventDefault();
+    const { data, error } = await supabase.auth.signUp({
+      email,
+      password,
+    });
+
+    if (!error) {
+      setIsSigningUp(true);
+    }
+    console.log({ data, error });
+  }
+
+  let signInMessage = "Sign In";
+
+  if (isSigningIn) {
+    signInMessage = "Signin In";
+  } else if (isNewUser) {
+    signInMessage = "Sign Up";
+  }
+
+  const signUpMessage = (
+    <p className="text-center text-white">
+      Email sent! Check your email to confirm sign up.
+    </p>
+  );
+
+  return (
+    <form
+      className="space-y-8"
+      onSubmit={isNewUser ? handleSignUp : handleLogin}
+    >
+      <input
+        type="email"
+        className="block w-full p-4 ps-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+        placeholder="Email"
+        required
+        onChange={(e) => setEmail(e.target.value)}
+        value={email}
+      />
+
+      <input
+        type="password"
+        className="block w-full p-4 ps-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+        placeholder="Password"
+        required
+        onChange={(e) => setPassword(e.target.value)}
+        value={password}
+      />
+
+      <button
+        type="submit"
+        className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800"
+      >
+        {signInMessage}
+      </button>
+      <p>
+        {isNewUser ? (
+          <>
+            Already have an account? {""}
+            <button onClick={() => setIsNewUser(false)} type="button">
+              Sign In
+            </button>
+          </>
+        ) : (
+          <>
+            Don't have an account? {""}
+            <button onClick={() => setIsNewUser(true)} type="button">
+              Sign Up
+            </button>
+          </>
+        )}
+      </p>
+      {isSigningUp && signUpMessage}
+    </form>
+  );
+}
